31e010318da8ff1c13d16774b43e872654d3bc77
ricky1994416 <56386394+ricky1994416@users.noreply.github.com>
2021-12-29 10:33:39 +0800

    Update base.js

--- tl.med.hku.hk/moodle-extend/base/js/base.js
+++ tl.med.hku.hk/moodle-extend/base/js/base.js
@@ -44,9 +44,9 @@ function addGlobalModal() {
 				var elementID = 'global-modal-'+size;
 				var className = 'modal-'+size;			
 				if ( !$('#'+elementID).length ) {		
-					$('
-						<div id="'+elementID+'" class="modal fade" data-backdrop="true" tabindex="-1" role="dialog" aria-modal="true">
-							<div class="modal-dialog '+className+'">
+					$("
+						<div id=""+elementID+"" class="modal fade" data-backdrop="true" tabindex="-1" role="dialog" aria-modal="true">
+							<div class="modal-dialog "+className+"">
 							
 								<div class="modal-content">
 									<div class="modal-header">
@@ -61,7 +61,7 @@ function addGlobalModal() {
 								</div>
 							</div>
 						</div>
-					').appendTo('body');
+					").appendTo('body');
 				}
 			});
 		}); // jquery-ready

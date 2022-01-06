d443504826dcb0f439d56ad29dbf460f1a4fde20
ricky1994416 <56386394+ricky1994416@users.noreply.github.com>
2022-01-06 16:29:55 +0800

    Update base.js

--- tl.med.hku.hk/moodle-extend/base/js/base.js
+++ tl.med.hku.hk/moodle-extend/base/js/base.js
@@ -60,7 +60,7 @@ function addGlobalModal() {
 								</div>
 							</div>
 						</div>
-					`).appendTo('#page-content');
+					`).appendTo('#region-main-box');
 				}
 			});
 		}); // jquery-ready
